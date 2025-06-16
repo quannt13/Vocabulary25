93b789729265dc25509f6de36848e7264e756e78
Nguyễn Tiến Q <84536863+quannt13@users.noreply.github.com>
2025-06-16 21:55:07 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,202 @@
+window.questions =[
+  {
+    "question": "Thợ thủ công trong tiếng Anh là gì?",
+    "correctAnswer": "A",
+    "answerA": "artisan",
+    "answerB": "electrician",
+    "answerC": "tourist",
+    "answerD": "helper"
+  },
+  {
+    "question": "Từ nào mang nghĩa 'cộng đồng'?",
+    "correctAnswer": "A",
+    "answerA": "community",
+    "answerB": "company",
+    "answerC": "committee",
+    "answerD": "common"
+  },
+  {
+    "question": "Người phục vụ cộng đồng gọi là gì?",
+    "correctAnswer": "A",
+    "answerA": "community helper",
+    "answerB": "police officer",
+    "answerC": "delivery person",
+    "answerD": "firefighter"
+  },
+  {
+    "question": "'Làng nghề thủ công' trong tiếng Anh là:",
+    "correctAnswer": "C",
+    "answerA": "handicraft shop",
+    "answerB": "craft house",
+    "answerC": "craft village",
+    "answerD": "pottery place"
+  },
+  {
+    "question": "Từ nào mang nghĩa 'cắt giảm'?",
+    "correctAnswer": "C",
+    "answerA": "get rid of",
+    "answerB": "cut off",
+    "answerC": "cut down on",
+    "answerD": "take away"
+  },
+  {
+    "question": "Nhân viên giao hàng là:",
+    "correctAnswer": "B",
+    "answerA": "postman",
+    "answerB": "delivery person",
+    "answerC": "collector",
+    "answerD": "helper"
+  },
+  {
+    "question": "Thợ điện tiếng Anh là:",
+    "correctAnswer": "A",
+    "answerA": "electrician",
+    "answerB": "engineer",
+    "answerC": "mechanic",
+    "answerD": "builder"
+  },
+  {
+    "question": "Từ nào mang nghĩa 'cơ sở vật chất'?",
+    "correctAnswer": "B",
+    "answerA": "materials",
+    "answerB": "facilities",
+    "answerC": "resources",
+    "answerD": "utilities"
+  },
+  {
+    "question": "'Firefighter' nghĩa là gì?",
+    "correctAnswer": "B",
+    "answerA": "cảnh sát",
+    "answerB": "người chữa cháy",
+    "answerC": "thợ sửa điện",
+    "answerD": "người đưa thư"
+  },
+  {
+    "question": "'Hương thơm' trong tiếng Anh là:",
+    "correctAnswer": "B",
+    "answerA": "flavor",
+    "answerB": "fragrance",
+    "answerC": "odor",
+    "answerD": "scented"
+  },
+  {
+    "question": "Từ nào nghĩa là 'chức năng'?",
+    "correctAnswer": "D",
+    "answerA": "feature",
+    "answerB": "work",
+    "answerC": "role",
+    "answerD": "function"
+  },
+  {
+    "question": "Người dọn vệ sinh được gọi là:",
+    "correctAnswer": "A",
+    "answerA": "cleaner",
+    "answerB": "garbage collector",
+    "answerC": "janitor",
+    "answerD": "delivery man"
+  },
+  {
+    "question": "Từ nào nghĩa là 'có quan hệ tốt với'?",
+    "correctAnswer": "C",
+    "answerA": "pass down",
+    "answerB": "hand down",
+    "answerC": "get on with",
+    "answerD": "look around"
+  },
+  {
+    "question": "'Truyền lại' có thể dùng từ nào sau đây?",
+    "correctAnswer": "B",
+    "answerA": "get on with",
+    "answerB": "pass down",
+    "answerC": "take on",
+    "answerD": "preserve"
+  },
+  {
+    "question": "Sản phẩm thủ công gọi là:",
+    "correctAnswer": "D",
+    "answerA": "product",
+    "answerB": "handmade",
+    "answerC": "craft",
+    "answerD": "handicraft"
+  },
+  {
+    "question": "'Thăm quan, ngắm xung quanh' là nghĩa của:",
+    "correctAnswer": "B",
+    "answerA": "preserve",
+    "answerB": "look around",
+    "answerC": "check in",
+    "answerD": "move around"
+  },
+  {
+    "question": "Từ nào nghĩa là 'nguyên bản'?",
+    "correctAnswer": "A",
+    "answerA": "original",
+    "answerB": "copy",
+    "answerC": "first",
+    "answerD": "authentic"
+  },
+  {
+    "question": "Từ nào sau đây cũng mang nghĩa 'truyền lại'?",
+    "correctAnswer": "A",
+    "answerA": "hand down",
+    "answerB": "give away",
+    "answerC": "send off",
+    "answerD": "pass on"
+  },
+  {
+    "question": "Công an gọi là gì trong tiếng Anh?",
+    "correctAnswer": "A",
+    "answerA": "police officer",
+    "answerB": "security guard",
+    "answerC": "detective",
+    "answerD": "military"
+  },
+  {
+    "question": "Từ nào sau đây nghĩa là 'đồ gốm'?",
+    "correctAnswer": "C",
+    "answerA": "ceramics",
+    "answerB": "clay",
+    "answerC": "pottery",
+    "answerD": "statue"
+  },
+  {
+    "question": "Từ nào sau đây có nghĩa là 'bảo tồn'?",
+    "correctAnswer": "C",
+    "answerA": "protect",
+    "answerB": "reserve",
+    "answerC": "preserve",
+    "answerD": "sustain"
+  },
+  {
+    "question": "Từ nào mang nghĩa 'hết, cạn kiệt'?",
+    "correctAnswer": "C",
+    "answerA": "run off",
+    "answerB": "use up",
+    "answerC": "run out of",
+    "answerD": "break down"
+  },
+  {
+    "question": "'Đặc sản' trong tiếng Anh là:",
+    "correctAnswer": "A",
+    "answerA": "speciality",
+    "answerB": "famous dish",
+    "answerC": "signature",
+    "answerD": "highlight"
+  },
+  {
+    "question": "Từ nào có nghĩa là 'vùng ngoại ô'?",
+    "correctAnswer": "B",
+    "answerA": "district",
+    "answerB": "suburb",
+    "answerC": "area",
+    "answerD": "region"
+  },
+  {
+    "question": "'Điểm du lịch' tiếng Anh là gì?",
+    "correctAnswer": "B",
+    "answerA": "tourist destination",
+    "answerB": "tourist attraction",
+    "answerC": "travel spot",
+    "answerD": "visit place"
+  }
+];
